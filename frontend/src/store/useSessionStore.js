555fd6016914043dcb31706edc881700cd78df29
Vidhitam Chakole <vidhitam.chakole@users.noreply.github.com>
2026-09-04 00:53:11 +0530

    feat: add visitor session reset

--- frontend/src/store/useSessionStore.js
+++ frontend/src/store/useSessionStore.js
@@ -3,6 +3,7 @@ import { create } from 'zustand'
 const useSessionStore = create((set) => ({
   visitor: '',
   setVisitor: (visitor) => set({ visitor }),
+  clearVisitor: () => set({ visitor: '' }),
 }))
 
 export default useSessionStore
